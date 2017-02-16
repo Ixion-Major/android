4585f1d6e1250f169f0b8bd4aa3f8404d6ed8c2a
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-02-16 22:50:50 +0530

    - initial commit
    -- setup firebase, buterknife and other libraries; add gitignores and setup

---
+++ app/src/main/java/com/flatmates/ixion/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.flatmates.ixion;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    @BindView(R.id.button_logout)
+    Button buttonlogout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.getCurrentUser();
+        buttonlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                Toast.makeText(getApplicationContext(), "Logout Successful", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
