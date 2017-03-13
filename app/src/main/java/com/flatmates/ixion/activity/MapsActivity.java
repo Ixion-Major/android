9ae040ded0f975c70c07dd6b2f9e80503f859cb1
Daman Singh <singhdaman4321@gmail.com>
2017-03-13 22:13:31 +0530

    feat: map working according to query

--- app/src/main/java/com/flatmates/ixion/activity/MapsActivity.java
+++ app/src/main/java/com/flatmates/ixion/activity/MapsActivity.java
@@ -3,6 +3,7 @@ package com.flatmates.ixion.activity;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -21,6 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import io.realm.Realm;
@@ -36,9 +38,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     String area;
-    String bedrooms;
+    String bhk;
     String city;
     String state;
+    String rent;
     double lat;
     double lon;
 
@@ -53,39 +56,87 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-//
-//        Bundle bundle = getIntent().getExtras().getBundle(Constants.KEY_BUNDLE);
-//        area = bundle.getString(KEY_AREA);
-//        bedrooms = bundle.getString(KEY_BEDROOMS);
-//        city = bundle.getString(KEY_CITY);
-//        state = bundle.getString(KEY_STATE);
+
+        Bundle bundle = getIntent().getExtras().getBundle(Constants.KEY_BUNDLE);
+        area = bundle.getString(KEY_AREA).toLowerCase();
+        bhk = bundle.getString(KEY_BEDROOMS);
+        city = bundle.getString(KEY_CITY).toLowerCase();
+        state = bundle.getString(KEY_STATE).toLowerCase();
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fetchData();
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        // Add a marker in Sydney and move the camera
+    }
+
+    private void showMarker(Double lati, Double loni) {
+        LatLng apna = new LatLng(lati, loni);
+        mMap.addMarker(new MarkerOptions().position(apna).title(bhk + "   " + area+"  "+city+"  "+state));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(apna, 10.0f));
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                double lon = marker.getPosition().longitude;
+                Query recentPostsQuery = myRefData.orderByChild("lon").equalTo(lon);
+                recentPostsQuery.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            Data data = snapshot.getValue(Data.class);
+
+//                            String email = data.getEmail();
+//                            String locality = data.getLocality();
+//                            int rent = data.getRent();
+//                            int noofrooms = data.getNoOfRooms();
+//                            new AlertDialog.Builder(MapsActivity.this)
+//                                    .setTitle("Information")
+//                                    .setMessage("Name: " + name + "\nRent: " + rent + "\nContact: " + email +
+//                                            "\nLocality: " + locality + "\nSize: " + noofrooms + "bhk")
+//                                    .show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                return false;
+            }
+        });
+    }
+
+    private void fetchData() {
 
         myRefData.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     Data data = snapshot.getValue(Data.class);
+                    String map_area = data.getArea();
+                    String map_city = data.getCity();
+                    String map_state = data.getState();
+                    String map_rent = data.getRent();
+                    String map_bhk = data.getBhk();
                     lat = Double.parseDouble(data.getLat());
                     lon = Double.parseDouble(data.getLon());
-                    LatLng apna = new LatLng(lat,lon);
-                    mMap.addMarker(new MarkerOptions().position(apna).title(lat + "   " + lon));
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(apna, 10.0f));
+                    System.out.println(map_city);
+                    if (area.equals(map_area) || city.equals(map_city) || state.equals(map_state)) {
+                        if (bhk.equals("null"))
+                            showMarker(lat, lon);
+                        else {
+                            if (bhk.equals(map_bhk))
+                                showMarker(lat, lon);
+                        }
+                    }
                 }
             }
 
@@ -94,5 +145,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Log.e("MainActivity", databaseError.getDetails());
             }
         });
+
+
     }
 }
--- app/src/main/java/com/flatmates/ixion/model/Data.java
+++ app/src/main/java/com/flatmates/ixion/model/Data.java
@@ -9,14 +9,54 @@ public class Data {
 
     String lat;
     String lon;
+    String area;
+    String bhk;
+    String city;
+    String rent;
+    String state;
 
     public Data(){
     }
 
-//    public Data(String lat, String lon) {
-//        this.lat = lat;
-//        this.lon = lon;
-//    }
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getBhk() {
+        return bhk;
+    }
+
+    public void setBhk(String bhk) {
+        this.bhk = bhk;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRent() {
+        return rent;
+    }
+
+    public void setRent(String rent) {
+        this.rent = rent;
+    }
 
     public String getLat() {
         return lat;
