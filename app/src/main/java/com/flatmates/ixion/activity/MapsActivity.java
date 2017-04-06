ec1e480c572851215fd309a23cf008c8ff0377fd
Daman Singh <singhdaman4321@gmail.com>
2017-04-06 22:00:48 +0530

    feat: add nearby places on marker's dialog button click

--- app/src/main/java/com/flatmates/ixion/activity/MapsActivity.java
+++ app/src/main/java/com/flatmates/ixion/activity/MapsActivity.java
@@ -2,8 +2,12 @@ package com.flatmates.ixion.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -12,15 +16,32 @@ import android.widget.Toast;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.flatmates.ixion.R;
 import com.flatmates.ixion.model.Data;
 import com.flatmates.ixion.model.UserMessage;
 import com.flatmates.ixion.utils.Constants;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
@@ -29,12 +50,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.location.LocationListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
 
 import static com.flatmates.ixion.utils.Constants.IS_USER_LOGGED_IN;
-import static com.flatmates.ixion.utils.Constants.IS_USER_ORDER_COMPLETE;
 import static com.flatmates.ixion.utils.Constants.KEY_ADDRESS;
 import static com.flatmates.ixion.utils.Constants.KEY_AREA;
 import static com.flatmates.ixion.utils.Constants.KEY_BEDROOMS;
@@ -49,21 +76,31 @@ import static com.flatmates.ixion.utils.Constants.KEY_RENT;
 import static com.flatmates.ixion.utils.Constants.KEY_STATE;
 import static com.flatmates.ixion.utils.Constants.USER_EMAIL;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener  {
 
+    private final String TAG = "MapsActivity";
     private GoogleMap mMap;
     String area = "";
     String bhk = "";
     String city = "";
     String state = "";
-    String rent = "";
     String name = "";
     String budget = "";
     int minBudget;
     int maxBudget;
     double lat;
     double lon;
+    double latitude, longitude;
     boolean dataFound;
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+
+    private static Location mLastLocation;
+    protected LocationRequest mLocationRequest;
+
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
 
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRefData = database.getReference("Data");
@@ -76,6 +113,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (checkPlayServicesAvailablity()) {
+
+            // Building the GoogleApi client
+            buildGoogleApiClient();
+        }
 
         //TODO: crashes here when you go to VR/userChat activity and come back -> Bundle is null
         Bundle bundle = getIntent().getExtras().getBundle(Constants.KEY_BUNDLE);
@@ -128,13 +171,166 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             fetchAB(state, bhk);
         else if (!bhk.equals(""))
             fetchBhk(bhk);
-
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private void showNearbyPlaces(String lat , String lon) {
+        try {
+            PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+            intentBuilder.setLatLngBounds( new LatLngBounds(
+                    new LatLng(Double.parseDouble(lat), Double.parseDouble(lon)),
+                    new LatLng(Double.parseDouble(lat), Double.parseDouble(lon))));
+            Intent intent = intentBuilder.build(this);
+            // Start the Intent by requesting a result, identified by a request code.
+            startActivityForResult(intent, 1);
+
+        } catch (GooglePlayServicesRepairableException e) {
+            GooglePlayServicesUtil
+                    .getErrorDialog(e.getConnectionStatusCode(), this, 0);
+        } catch (GooglePlayServicesNotAvailableException e) {
+            Toast.makeText(this, "Google Play Services is not available.",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    private void displayLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Snackbar.make(findViewById(android.R.id.content), "Please provide location permission.", Snackbar.LENGTH_INDEFINITE).show();
+            return;
+        }
+        try{
+            startLocationUpdates();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+            LatLng apna = new LatLng(latitude, longitude);
+            mMap.addMarker(new MarkerOptions().position(apna).title("You are here").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+        } else {
+            Toast.makeText(this, "Couldn't get current location. Make sure location is enabled on the device", Toast.LENGTH_SHORT).show();
+            latitude = 28.567333;
+            longitude = 77.318373;
+
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+        createLocationRequest();
+    }
+
+
+    private boolean checkPlayServicesAvailablity() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServicesAvailablity();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        try {
+            startLocationUpdates();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        // Once connected with google api, get the location
+        displayLocation();
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(this, "Cant update location. Provide storage permission.", Toast.LENGTH_SHORT).show();
+
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                mLastLocation = location;
+            }
+        });
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(1000000000);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(100000000);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
     }
 
     @Override
@@ -143,8 +339,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-                double lon = marker.getPosition().longitude;
-                Query recentPostsQuery = myRefData.orderByChild("lon").equalTo(String.valueOf(lon));
+                final String lon = String.valueOf(marker.getPosition().longitude);
+                final String lat = String.valueOf(marker.getPosition().latitude);
+                Query recentPostsQuery = myRefData.orderByChild("lon").equalTo(lon);
                 recentPostsQuery.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
@@ -166,6 +363,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                     .title(name)
                                     .content("Rent: " + rent + "\nNo. of Rooms: "+ bhk+"\nAddress: " + address)
                                     .positiveText("SHOW MORE")
+                                    .negativeText("SHOW NEARBY PLACES")
+                                    .onNegative(new MaterialDialog.SingleButtonCallback() {
+                                        @Override
+                                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                            showNearbyPlaces(lat, lon);
+                                        }
+                                    })
                                     .onPositive(new MaterialDialog.SingleButtonCallback() {
                                         @Override
                                         public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
@@ -240,17 +444,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     }
                 }
                 if (!dataFound) {
-                    new MaterialDialog.Builder(MapsActivity.this)
-                            .title("Oops!")
-                            .content("No result found")
-                            .positiveText("OK")
-                            .onPositive(new MaterialDialog.SingleButtonCallback() {
-                                @Override
-                                public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                                    onBackPressed();
-                                }
-                            })
-                            .show();
+                    noResultDialog();
                 }
             }
 
@@ -283,17 +477,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     }
                 }
                 if (!dataFound) {
-                    new MaterialDialog.Builder(MapsActivity.this)
-                            .positiveText("OK")
-                            .title("Oops!")
-                            .content("No result found")
-                            .onPositive(new MaterialDialog.SingleButtonCallback() {
-                                @Override
-                                public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                                    onBackPressed();
-                                }
-                            })
-                            .show();
+                    noResultDialog();
                 }
             }
 
@@ -328,17 +512,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     }
                 }
                 if (!dataFound) {
-                    new MaterialDialog.Builder(MapsActivity.this)
-                            .title("Oops!")
-                            .positiveText("OK")
-                            .content("No result found")
-                            .onPositive(new MaterialDialog.SingleButtonCallback() {
-                                @Override
-                                public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                                    onBackPressed();
-                                }
-                            })
-                            .show();
+                    noResultDialog();
                 }
             }
 
@@ -372,17 +546,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     }
                 }
                 if (!dataFound) {
-                    new MaterialDialog.Builder(MapsActivity.this)
-                            .title("Oops!")
-                            .positiveText("OK")
-                            .content("No result found")
-                            .onPositive(new MaterialDialog.SingleButtonCallback() {
-                                @Override
-                                public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                                    onBackPressed();
-                                }
-                            })
-                            .show();
+                    noResultDialog();
                 }
             }
 
@@ -417,17 +581,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         }
                     }
                     if (!dataFound) {
-                        new MaterialDialog.Builder(MapsActivity.this)
-                                .title("Oops!")
-                                .positiveText("OK")
-                                .content("No result found")
-                                .onPositive(new MaterialDialog.SingleButtonCallback() {
-                                    @Override
-                                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                                        onBackPressed();
-                                    }
-                                })
-                                .show();
+                        noResultDialog();
                     }
                 }
             }
@@ -438,5 +592,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+    }
+
+    private void noResultDialog(){
+        new MaterialDialog.Builder(MapsActivity.this)
+                .title("Oops!")
+                .positiveText("OK")
+                .content("No result found")
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        onBackPressed();
+                    }
+                })
+                .show();
     }
 }
