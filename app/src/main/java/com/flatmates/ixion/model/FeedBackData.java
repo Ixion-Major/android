b9689864db07794791491c27e58d0f94c677ede3
Daman Singh <singhdaman4321@gmail.com>
2017-04-22 23:50:24 +0530

    feat: add feedback form

---
+++ app/src/main/java/com/flatmates/ixion/model/FeedBackData.java
@@ -0,0 +1,26 @@
+package com.flatmates.ixion.model;
+
+/**
+ * Created by daman on 22/4/17.
+ */
+
+public class FeedBackData {
+
+    String title, description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
